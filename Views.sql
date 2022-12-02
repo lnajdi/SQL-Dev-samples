83cc299ea8518387e32bb9533709e81b51f32209
lnajdi <l.najdi@uiz.ac.ma>
2022-12-02 07:50:32 +0000

    Add Views demo

---
+++ Views.sql
@@ -0,0 +1,75 @@
+
+create or replace VIEW view_emp_sal_max
+  as   
+SELECT e.last_name, e.salary, e.department_id, d.maxsal
+FROM employees e,
+        (SELECT department_id, max(salary) maxsal
+        FROM employees
+        GROUP BY department_id) d
+WHERE e.department_id= d.department_id
+AND e.salary= d.maxsal;
+
+
+select * from view_emp_sal_max ;
+
+select * from user_views;
+
+select view_name ,text from user_views;
+--drop view EMP_DETAILS_VIEW;
+
+DESCRIBE employees;
+desc view_emp_sal_max;
+
+
+select table_name from user_tables
+where table_name like '%EMPL%';
+
+drop table copy_employees;
+create table copy_employees as select * from employees;
+
+desc copy_employees;
+
+select count(*) from copy_employees;
+
+
+CREATE OR replace VIEW view_dept50
+AS SELECT department_id, employee_id, 
+        first_name, last_name, salary
+    FROM copy_employees
+    WHERE department_id= 50;
+
+
+select employee_id , department_id 
+from  view_dept50 where employee_id = 124;
+
+select count(*) from view_dept50;
+
+UPDATE view_dept50
+SET department_id= 90
+WHERE employee_id= 124;
+
+
+
+CREATE OR replace VIEW view_dept50
+AS SELECT department_id, employee_id, 
+        first_name, last_name, salary
+    FROM copy_employees
+    WHERE department_id= 50
+with check option CONSTRAINT con_view_dept50 ;
+
+
+select employee_id , department_id 
+from  view_dept50 where employee_id = 124;
+
+UPDATE view_dept50
+SET department_id= 90
+WHERE employee_id= 124;
+
+
+select * from user_constraints
+where constraint_name = upper('con_view_dept50');
+
+desc employees;
+select * from user_tables 
+where table_name = 'EMPLOYEES';
+
